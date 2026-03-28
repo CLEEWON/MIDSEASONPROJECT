1db358a5effa2bc1788dcbf3fe35e56f733a3c52
nazwalia <aulianasua@gmail.com>
2026-03-28 12:06:08 +0700

    make header

---
+++ resources/views/Components/header.blade.php
@@ -0,0 +1,4 @@
+<header class="bg-white shadow p-4 flex justify-between items-center">
+    <h1 class="text-xl font-bold text-gray-700">Admin Panel</h1>
+    <div class="text-sm text-gray-500">Welcome, Admin</div>
+</header>
